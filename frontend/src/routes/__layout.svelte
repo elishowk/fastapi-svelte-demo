6bc5c6ad0523a2bb883e68fa38c98964e366c90a
elias@showk.me <elias@huggingface.co>
2021-11-16 15:59:52 +0100

    add tailwindcss and postcss

---
+++ frontend/src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<nav class="my-4">
+	<a class="text-center text-3xl" href="/">Home</a>
+	<a class="text-center text-3xl" href="/about">About</a>
+</nav>
+
+<slot />
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to this demo using SvelteKit, FastAPI and Piccolo ORM</h1>
+<p>Visit <a href="https://github.com/elishowk/fastapi-svelte-demo">elishowk/fastapi-svelte-demo</a> to read the documentation</p>
